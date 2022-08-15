0e703ac531a62a3f205a97b69763665ce6fb4c16
Phiwa Madinga <phiwa@koedr.org>
2022-08-15 17:18:11 +0200

    Update switch color and styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,29 @@
+
 import React, { useState } from 'react';
 import './App.css';
 
 
 const colors: string[] = [
-  '#D1F0B1', //tea green
+  '#4D9DE0', //Carolina blue
   '#306B34', //Darthmouth green
   '#1C5253', //Deep jungle green
-  '#E59F71', //Tan crayola
+  '#A31621', //Ruby red
   '#81667A' //old lavender
 ]
 
 const App = () => {
   const [color, setColor] = useState('#D1F0B1')
+  const [currentColorIndex, setCurrentColorIndex] = useState(0)
 
   const handleClick = (colors:string[]) =>{
-    setColor(colors[3])
+    setColor(colors[currentColorIndex])
+    console.log(currentColorIndex);
+    
+    if(currentColorIndex >= 4){
+      setCurrentColorIndex(0)
+    }else {
+      setCurrentColorIndex(currentColorIndex + 1)
+    }
   }
   return (
     <>
@@ -25,9 +34,13 @@ const App = () => {
       </header>
 
       <main>
-        <button
-          style={{backgroundColor: color}}
-          color='red' onClick={() => handleClick(colors)}>Test Btn</button>
+        <div className="color-btn">
+          <button
+            style={{backgroundColor: color}}
+            color='red' onClick={() => handleClick(colors)}>
+              Test Btn
+          </button>
+        </div>
       </main>
     </>
   );
